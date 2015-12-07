769f57defe75ca827957a7e98f8af92ee28a5f23
StephenBrikiatis <stephen.brikiatis@mymail.champlain.edu>
2015-12-07 12:14:02 -0500

    Update transaction.h

--- transaction.h
+++ transaction.h
@@ -1,1 +1,19 @@
 //Stephen
+#include "common.h"
+#include "LinkedList.h"
+
+struct Transaction
+{
+  private:
+    LinkedList tItems;
+    bool relevant;
+  public:
+    Transaction();
+    Transaction(Linkedlist tItems, bool relevant
+    ~Transaction();
+    
+    void addItem(int item);
+    
+    bool getRelavent();
+    void setRelavent(bool newRelavent);
+};
