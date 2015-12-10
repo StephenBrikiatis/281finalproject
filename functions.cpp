74fe95dd99130fa476b87992c3b5dd7dd5aa46eb
StephenBrikiatis <stephen.brikiatis@mymail.champlain.edu>
2015-12-09 20:38:03 -0500

    create list of numbers

--- functions.cpp
+++ functions.cpp
@@ -19,5 +19,45 @@ bool checkUserInput(string input)
 
 void createBasket(CorrelationBasket currentBasket, int comboLength)
 {
-	N = 
+	LinkedList<int> numbers;
+	int numbersLength;
+
+	numbers = createListOfNums(currentBasket);
+	numbersLength = numbers.getCount();
+
+	if(comboLength > numbersLength)
+	{
+		cout << "Can't make a combination with that few numbers" << endl;
+		return;
+	}
+
+
+}
+
+LinkedList<int> createListOfNums(CorrelationBasket currentBasket)
+{
+	LinkedList<int> listOfNums;
+	int basketSize = 0, correSize = 0, compare;
+	Correlation tmp;
+	bool exists = false;
+
+	basketSize = currentBasket.getSize();
+	tmp = currentBasket.getCorrelation(0);
+	correSize = tmp.getSize();
+
+	for(int i = 0; i < basketSize; i++)
+	{
+		tmp = currentBasket.getCorrelation(i);
+		for(int j = 0; j < correSize; j++)
+		{
+			compare = tmp.getItem(j);
+			for(int k = 0; k < listOfNums.getCount(); k++)
+			{
+				listOfNums.getData(k);
+			}
+			listOfNums.insert(compare);
+		}
+	}
+
+	return listOfNums;
 }
