f8806f3f7a36e73dd90612290a944b7518f10778
gabepereyra <gabrielpereyra24@gmail.com>
2015-12-11 00:54:58 -0500

    Quick Menu logic update

--- main.cpp
+++ main.cpp
@@ -1,50 +1,68 @@
 #include "Header.h"
 
-//int main()
-//{
-//	//initial variables
-//	int minimumOccurance; //variable for setting the minimum occurance that will be checked for
-//	bool exit = false;
-//	string fileName;
-//
-//	TransactionBasket mainTransactions;
-//	TransactionBasket otherBasket;
-//
-//	cout << "Welcome to New Horizon One's Apriori system. \n \n";
-//
-//	//menu loop
-//	while (!exit)
-//	{	
-//		cout << "Please input the filename you wish to mine for, or enter -1 to leave. \n \n";
-//		cout << "Input here: ";
-//
-//		cin >> fileName;
-//
-//		if (fileName == "-1")
-//		{
-//			cout << endl << endl << "Have a nice day!" << endl;
-//			exit = true;
-//		}
-//		else if (checkUserInput(fileName))
-//		{
-//			//DO APRIORI STUFF HERE
-//			mainTransactions = TransactionBasket("dataset/"+ fileName + ".txt");
-//
-//			int test;
-//			test = mainTransactions.getTransaction(0).getItem(0);
-//			cout << "test result: " << test << endl;
-//
-//			cout << "Debugging survived!" << endl << endl;
-//		}
-//		else
-//		{
-//			cout << endl << "Sorry, the file you requested was not in our database. Please try again." << endl << endl;
-//		}
-//	}
-//
-//	system("pause");
-//	return 0;
-//}
+int main()
+{
+	//initial variables
+	int minimumOccurance; //variable for setting the minimum occurance that will be checked for
+	bool exit = false;
+	string fileName;
+
+	Transaction mainTransactions[1000];
+	int transactionSize;
+	Correlation mainCorrelations[100000];
+	int correlationSize;
+
+	cout << "Welcome to New Horizon One's Apriori system. \n \n";
+
+	//menu loop
+	while (!exit)
+	{	
+		cout << "Please input the filename you wish to mine for, or enter -1 to leave. \n \n";
+		cout << "Input here: ";
+
+		cin >> fileName;
+
+		if (fileName == "-1")
+		{
+			cout << endl << endl << "Have a nice day!" << endl;
+			exit = true;
+		}
+		else if (checkUserInput(fileName))
+		{
+			//DO APRIORI STUFF HERE
+
+			//following two functions will load initial arrays
+			mainTransactions.populateWithFile(string);
+			mainCorrelations.populateFromTrans(mainTransactions);//this function needs to be made
+
+			//main apriori loop
+			while (correlationSize > 0)
+			{
+
+				//this function will check how many times each correlation occurs
+				updateOccurances(mainCorrelations, correlationsize, mainTransactions, transactionsize);
+
+				//PRUNE CORRELATIONS HERE, FUNCTION HAS YET TO BE DEFINED
+				createBasket / createListOfNums
+
+				//PRUNE TRANSACTIONS HERE
+				compare(mainTransactions)
+
+				//Output results and time!
+				printCorrelations()
+			}
+
+			//REPEAT UNTIL NO MORE CORRELATIONS
+		}
+		else
+		{
+			cout << endl << "Sorry, the file you requested was not in our database. Please try again." << endl << endl;
+		}
+	}
+
+	system("pause");
+	return 0;
+}
 
 int main()
 {
@@ -91,18 +109,18 @@ int main()
 	return 0;*/
 }
 
-int main() //yet another main!!!
-{
-	Correlation cHolder;
-	Transaction tHolder;
-	int input;
-	string userInput;
-	
-	do
-	{
-		cout << "Enter file name: ";
-		cin >> userInput;
-
-	} while (userInput != "exit");
-
-}
+//int main() //yet another main!!!
+//{
+//	Correlation cHolder;
+//	Transaction tHolder;
+//	int input;
+//	string userInput;
+//	
+//	do
+//	{
+//		cout << "Enter file name: ";
+//		cin >> userInput;
+//
+//	} while (userInput != "exit");
+//
+//}
